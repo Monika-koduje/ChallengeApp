dbcf06cdf9aa36d765d467822cc6dd3a8ec2e7a3
Monika-koduje <m.osenkowska1@gmail.com>
2023-05-12 01:24:11 +0200

    tiny program about age, name and gender

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello Everyone! Have a nice day ♥");
+﻿string name = "Monika";
+bool female = true;
+int age = 100;
+
+if (female && age > 30)
+{
+    Console.WriteLine(name + "," + " " + "lat" + " " + age);
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+
