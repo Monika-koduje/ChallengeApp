80290a17643fffee629ff506af3ef33a48185928
Monika-koduje <m.osenkowska1@gmail.com>
2023-05-12 23:04:30 +0200

    Counter of digit in number

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,32 +1,75 @@
-﻿string name = "Monika";
-bool female = true;
-int age = 100;
+﻿int number = 5561213;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (female && age > 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    if (name == "Ewa" && age == 33)
+    if (letter == '0')
     {
-        Console.WriteLine("Ewa, lat 33");
+        counter0++;
     }
-    else
+    else if (letter == '1')
     {
-        Console.WriteLine(name + ", lat " + age);
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
     }
 }
-else if (!female)
-{
-    if (age < 18)
-    {
-        Console.WriteLine("Niepełnoletni Mężczyzna");
-    }
-    else
-    {
-        Console.WriteLine("Pełnoletni Mężczyzna");
-    }
-}
-else
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
 
+string text = "Licznik powtarzających się cyfr w podanej liczbie";
+string textUpper = text.ToUpper();
 
+Console.WriteLine("\n " + textUpper + "\n\n Wyniki dla liczby: " + number);
+
+Console.WriteLine("\n\n Cyfra => Ilość\n");
+Console.WriteLine(" 0 => " + counter0);
+Console.WriteLine(" 1 => " + counter1);
+Console.WriteLine(" 2 => " + counter2);
+Console.WriteLine(" 3 => " + counter3);
+Console.WriteLine(" 4 => " + counter4);
+Console.WriteLine(" 5 => " + counter5);
+Console.WriteLine(" 6 => " + counter6);
+Console.WriteLine(" 7 => " + counter7);
+Console.WriteLine(" 8 => " + counter8);
+Console.WriteLine(" 9 => " + counter9);
