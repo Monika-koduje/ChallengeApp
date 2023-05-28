5d4a66f7557356da9456c7ff872fb7c6d35036b5
Monika-koduje <m.osenkowska1@gmail.com>
2023-05-28 23:30:56 +0200

    Added tests compare value types and reference types

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void WhenTypeIsInt_ThenWeHaveValueType()
+        {
+
+            int age1 = 12;
+            int age2 = 12;
+
+            Assert.AreEqual(age1, age2);
+        }
+
+        [Test]
+        public void WhenTypeIsObjectClass_ThenWeHaveReferenceType()
+        {
+
+            var age1 = Age("Monika", "B", 12);
+            var age2 = Age("Kasia", "A", 12);
+
+
+            Assert.AreEqual(age1.Age, age2.Age);
+
+        }
+        private Employee Age(string NewName, string NewSurname, int NewAge)
+        {
+            return new Employee(NewName, NewSurname, NewAge);
+        }
+
+
+    }
+}
